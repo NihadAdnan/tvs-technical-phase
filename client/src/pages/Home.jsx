4c13b0a7530fa2d60f47c50a0afcee775f1c6247
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-08-15 02:30:16 +0600

    creating Home page UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,22 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
-        <Header/>
-        <h1>Home Page</h1>
+    <div className="min-h-screen bg-white">
+      <Header/>
+      {/* Hero Section */}
+      <div className="flex flex-col items-center justify-center text-center py-40 space-y-8">
+      <div className="flex flex-col items-center justify-center gap-7">
+        <div className="flex items-center gap-7">
+          <h1 className="text-7xl font-normal text-black">WELCOME</h1>
+          <div className="px-6 py-2 bg-gradient-to-r from-blue-600 to-purple-400 text-white text-6xl font-bold rounded-full shadow-md">
+            To TVS HRMS
+          </div>
+        </div>
+        <button className="text-purple-600 border-4 border-purple-600 rounded-full px-10 py-4 text-4xl font-semibold transition-all duration-300 hover:bg-purple-600 hover:text-white">
+          Download Documents
+        </button>
+      </div>
+    </div>
     </div>
   ) 
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import logo from "./../assets/logo.png";
+import icon from "./../assets/icon.png";
 import { useState } from 'react';
 import {useNavigate} from 'react-router-dom'
 
@@ -70,7 +71,7 @@ export default function SignUp() {
           </div>
           <div>
             <button type="button" className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-900 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-              <img src="path/to/google-logo.png" alt="Google" className="h-5 w-5 mr-2" />
+              <img src={icon} alt="Google" className="h-5 w-5 mr-2" />
               Login in with Google
             </button>
           </div>
