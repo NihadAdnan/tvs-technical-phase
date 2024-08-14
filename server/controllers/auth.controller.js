12ec639a75303e11b57b4ee595edc35d2c04ae1e
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-08-15 01:42:19 +0600

    created signin api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
 
-export const signup = async (req, res, next) => {
+export const signup = async (req, res) => {
     const { username, email, password } = req.body;
     const hashedpassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedpassword });
@@ -10,6 +10,18 @@ export const signup = async (req, res, next) => {
       await newUser.save();
       res.status(201).send({ message: "User created successfully!" });
     } catch (error) {
-      next(error);
+
+    }
+  };
+
+  export const signin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return res.status(400).json('Invalid Credentials!')
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return res.status(401).json('Wrong credentials!');
+      res.status(200).send({ message: "Logged in successfully!" });
+    } catch (error) {
     }
   };
