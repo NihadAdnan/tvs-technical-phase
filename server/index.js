23f8fcbda9529c0d966598c1818a2f2b06c88fe1
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-08-15 02:53:24 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express  from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userAuth from "../server/routes/auth.route.js";
+import path  from "path";
 
 dotenv.config()
 const app = express();
@@ -14,8 +15,18 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err)
 })
 
+
+const __dirname = path.resolve()
+
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000!")
 })
 
 app.use("/api/auth", userAuth)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
